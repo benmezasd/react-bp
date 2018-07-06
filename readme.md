129e030834b6f956798e50441738dfa6dbf42d49
Ben Meza <benjamin.meza@millenniumhealth.com>
2018-07-06 13:25:50 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# React Boilerplate
+
+Repository to use to slap something together really fast with all dependencies needed for creating an application
+
+## What's in the box?!?!?!?!
+
+
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index-bundle.js',
+  },
+  resolve: {
+    extensions: ['.es6', '.js', '.jsx'],
+    modules: ['node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader', // compiles Sass to CSS
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
